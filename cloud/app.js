f99044021dc9862ae8cf6cb8bf1d525b6d5b5092
Craig R. Hughes <craig.git@rungie.com>
2014-05-21 14:12:41 -0700

    Implement inventory list

--- cloud/app.js
+++ cloud/app.js
@@ -24,7 +24,7 @@ app.get('/login', function(req, res) { res.render('login'); });
 app.post('/login', function(req, res) {
     Parse.User.logIn(req.body.username, req.body.password).then(function() {
         // Check if we have somewhere to go; if so, go there
-        var redirect_to = req.session.postLoginPage ? req.session.postLoginPage : '/prices';
+        var redirect_to = req.session.postLoginPage ? req.session.postLoginPage : '/inventory';
         delete req.session.postLoginPage;
         res.redirect(redirect_to);
     },
@@ -51,6 +51,8 @@ function IsAuthenticated(req,res,next)
 // The homepage renders differently depending on whether user is logged in.
 app.get('/', IsAuthenticated, function(req, res) { res.redirect('/prices'); });
 
+// PRICES
+// ======
 app.get('/prices', IsAuthenticated, function(req, res) {
     // Display the user profile if user is logged in.
     if (Parse.User.current()) {
@@ -62,14 +64,47 @@ app.get('/prices', IsAuthenticated, function(req, res) {
                 Parse.User.current().fetch();
                 res.render('prices', { user:Parse.User.current(), prices: prices });
             },
-            error: function() {
-              response.error("Prices lookup failed");
+            error: function(err) {
+              res.json(500, err);
             }
         });
     } else {
         // User not logged in, redirect to login form.
         res.redirect('/login');
     }
+});
+
+// INVENTORY
+// =========
+app.get('/inventory', function(req, res) {
+    // If user exists, fetch their info for display
+    if(Parse.User.current()) { Parse.User.current().fetch(); }
+
+    // Query for Meat that is in the price list
+    var meatQuery = new Parse.Query('Meat')
+        .include('cut')
+        .include('freezer')
+        .include('animal');
+
+    // Parse.Query.ascending(xyz) won't let you sort by sub-object fields like cut.species,
+    // so we form a collection and sort using a comparator
+    var meats = meatQuery.collection();
+    meats.comparator = function(object)
+    {
+        var cut = object.get('cut');
+        // Use | as separator because at least in ASCII it's alphabetically later than all letters
+        return cut.get('species')+'|'+cut.get('category')+'|'+cut.get('cut');
+    };
+    meats.fetch({
+        success: function(meats)
+        {
+            res.render('inventory', { user: Parse.User.current(), meats: meats });
+        },
+        error: function(err)
+        {
+            res.json(500, err);
+        }
+    });
 });
 
 // Error handler
