19575bfe0486f31ce1e23dfd40fb5198ae05c856
Craig R. Hughes <craig.git@rungie.com>
2014-05-20 17:24:55 -0700

    Add gitignore and Sublime project; update to latest version

--- cloud/app.js
+++ cloud/app.js
@@ -14,20 +14,19 @@ app.set('view engine', 'ejs');    // Set the template engine
 app.use(parseExpressHttpsRedirect());  // Require user to be on HTTPS.
 app.use(express.bodyParser());
 app.use(express.cookieParser('Beef is good'));
-app.use(parseExpressCookieSession({ cookie: { maxAge: 3600000 } }));
+app.use(express.cookieSession( {secret:'OMG so good'} ));
+app.use(parseExpressCookieSession({ cookie: { maxAge: 3600000 }, fetchUser: true }));
 
-// You could have a "Log In" link on your website pointing to this.
-app.get('/login', function(req, res) {
-    // Renders the login form asking for username and password.
-    res.render('login.ejs');
-});
+// Renders the login form asking for username and password.
+app.get('/login', function(req, res) { res.render('login.ejs'); });
 
 // Clicking submit on the login form triggers this.
 app.post('/login', function(req, res) {
     Parse.User.logIn(req.body.username, req.body.password).then(function() {
-        // Login succeeded, redirect to homepage.
-        // parseExpressCookieSession will automatically set cookie.
-        res.redirect('/inventory');
+        // Check if we have somewhere to go; if so, go there
+        var redirect_to = req.session.postLoginPage ? req.session.postLoginPage : '/inventory';
+        delete req.session.postLoginPage;
+        res.redirect(redirect_to);
     },
     function(error) {
         // Login failed, redirect back to login form.
@@ -35,33 +34,33 @@ app.post('/login', function(req, res) {
     });
 });
 
-// You could have a "Log Out" link on your website pointing to this.
-app.get('/logout', function(req, res) {
-    Parse.User.logOut();
-    res.redirect('/');
-});
+app.get('/logout', function(req, res) { Parse.User.logOut(); res.redirect('/'); });
 
-// The homepage renders differently depending on whether user is logged in.
-app.get('/', function(req, res) {
+function IsAuthenticated(req,res,next)
+{
+    // Check that user is logged in, and redirect to login if not
     if(Parse.User.current())
     {
-        res.redirect('/inventory');
-    }
-    else
-    {
+        next();
+    } else {
+        req.session.postLoginPage = req.url;
         res.redirect('/login');
     }
-});
+}
 
-app.get('/inventory', function(req, res) {
+// The homepage renders differently depending on whether user is logged in.
+app.get('/', IsAuthenticated, function(req, res) { res.redirect('/inventory'); });
+
+app.get('/inventory', IsAuthenticated, function(req, res) {
     // Display the user profile if user is logged in.
     if (Parse.User.current()) {
-        var query = new Parse.Query('Prices');
-        query.ascending('species,category,cut');
+        var query = new Parse.Query('Prices')
+            .ascending('-species,category,available,cut')
 
-        query.find({
+        .find({
             success: function(prices) {
-                res.render('inventory', { prices: prices });
+                Parse.User.current().fetch();
+                res.render('inventory', { user:Parse.User.current(), prices: prices });
             },
             error: function() {
               response.error("Prices lookup failed");
@@ -71,6 +70,12 @@ app.get('/inventory', function(req, res) {
         // User not logged in, redirect to login form.
         res.redirect('/login');
     }
+});
+
+// Error handler
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.send(500, 'Something broke!');
 });
 
 app.listen();
--- cloud/main.js
+++ cloud/main.js
@@ -1,1 +1,125 @@
 require('cloud/app.js');
+
+var Prices = Parse.Object.extend('Prices')
+var Meat = Parse.Object.extend('Meat')
+var Animal = Parse.Object.extend('Animal')
+var Freezer = Parse.Object.extend('Freezer')
+
+Parse.Cloud.define('addMeat',function(req, res)
+{
+    var freezer = new Freezer()
+    freezer.id = 'jUUUx5dgRn'
+
+    var animal = new Animal()
+    animal.id = 'djQr2snFz7'
+
+    var newMeat = new Meat();
+    newMeat.save({
+        cut:        req.params.cut,
+        weight:     req.params.weight,
+        location:   req.params.location,
+        freezer:    freezer,
+        animal:     animal,
+    })
+    .then(function()
+    {
+        res.success('OK')
+    },
+    function(err)
+    {
+        res.error(err)
+    })
+})
+
+/*
+Parse.Cloud.beforeSave('Meat', function(req, res)
+{
+    console.log('Starting beforeSave')
+    if(!req.object.isNew())
+    {
+        // Remove the old weight from the old price
+        new Parse.Query(Meat)
+        .include('animal.species')
+        .get(req.object.id)
+        .then(function(oldMeat)
+        {
+            new Parse.Query(Prices)
+            .equalTo('species', oldMeat.get('animal').get('species'))
+            .equalTo('cut', oldMeat.get('cut'))
+            .first()
+            .then(function(price)
+            {
+                price.decrement('available', oldMeat.get('weight'))
+                .save()
+                .then(function(){console.log("Saved old price")});
+            },
+            function(noprice) {console.log("There was no price for old meat")}  // If there was no price, ignore
+            )
+        },
+        function(err)
+        {
+            console.error('Very very odd: could not locate existing Meat with id: ' + req.object.id + ' : ' + err.message);
+            res.error('Very very odd: could not locate existing Meat with id: ' + req.object.id + ' : ' + err.message);
+        });
+    }
+
+    req.object.get('animal').fetch()
+    .then(function()
+    {
+        // Now add the new weight to the
+        new Parse.Query(Prices)
+        .equalTo('species', req.object.get('animal').get('species'))
+        .equalTo('cut', req.object.get('cut'))
+        .first()
+        .then(function(price)
+            {
+                return price;
+            },
+            function(error)
+            {
+                return new Prices();
+            }
+        )
+        .then(function(price)
+        {
+            price.increment('available', req.object.weight)
+            .save()
+            .then(function(){console.log('Saved new price');});
+        })
+        .then(function()
+        {
+            res.success()
+        })
+    })
+
+    console.error('Reached end of beforeSave')
+})
+*/
+
+Parse.Cloud.beforeDelete('Meat', function(req, res)
+{
+    req.object.get('animal')[0].fetch()
+    .then(function(animal)
+    {
+        console.log('Species is ' + animal.get('species'))
+
+        new Parse.Query(Prices)
+        .equalTo('species', animal.get('species'))
+        .equalTo('cut', req.object.get('cut'))
+        .first()
+        .then(function(price)
+        {
+            price.increment('available', -req.object.get('weight')).save()
+            .then(function()
+            {
+                console.log('Price saved OK')
+                res.success()
+            })
+        })
+    },
+    function(err)
+    {
+        console.error('We got an error saving price??!?' + err.code + ' : ' + err.message)
+        res.error(err.message)
+    })
+})
