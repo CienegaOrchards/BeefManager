76932d608388c4530a439cb239b9d2b6eb626e70
Craig R. Hughes <craig.git@rungie.com>
2014-06-04 12:39:45 -0700

    Add cloud functions for:
        getOrMakeAnimal
        getOrMakePrice
        makeMeat

--- cloud/meat.js
+++ cloud/meat.js
@@ -5,6 +5,78 @@ var Freezer = Parse.Object.extend('Freezer');
 var Order = Parse.Object.extend('Order');
 var OrderItem = Parse.Object.extend('OrderItem');
 
+// Function: makeMeat
+// @param {String} req.params.species            The species of the meat, eg. "Beef" or "Chicken"
+// @param {String} req.params.cut                The cut of the meat, eg. "Rump Roast" or "Stewing Hen"
+// @param {Number} req.params.units              The number of units of meat, eg. 3.0 for 3.0 lb or 1 for 1 hen; Unit type depends on what the cut was and is defined when creating the cut
+// @param {String} req.params.butcheryDate       The date on which the meat was butchered, eg. "2014-06-04"
+// @param {String} [req.params.animalIdentifier] The 'identifier' field for the animal, eg. "Bessy" or "XYZ12345"
+// @param {String} [req.params.category]         The category of this cut, eg. "Roasts" or "Organic"
+// @param {String} [req.params.unitType]         The type of the units for this cut, eg. "lb" or "hen"
+// @param {Number} [req.params.pricePerUnit]     The price per unit, eg. 15.5 for "$15.50 per lb"
+Parse.Cloud.define('makeMeat', function(req,res)
+{
+    if(req.params.species === undefined)
+    {
+        res.json(400, new Parse.Error(Parse.Error.MISSING_OBJECT_ID, 'Failed to specify species'));
+    }
+    else if(req.params.cut === undefined)
+    {
+        res.json(400, new Parse.Error(Parse.Error.MISSING_OBJECT_ID, 'Failed to specify cut'));
+    }
+    else if(req.params.units === undefined)
+    {
+        res.json(400, new Parse.Error(Parse.Error.MISSING_OBJECT_ID, 'Failed to specify units'));
+    }
+    else if(req.params.butcheryDate === undefined)
+    {
+        res.json(400, new Parse.Error(Parse.Error.MISSING_OBJECT_ID, 'Failed to specify butcherDate'));
+    }
+    else
+    {
+        // Find the Animal if it exists, and if not, create it
+        Parse.Cloud.run('getOrMakeAnimal', { identifier: req.params.animalIdentifier, species: req.params.species, butcheryDate: req.params.butcheryDate },
+        {
+            success: function(animalRes)
+            {
+                // Find the Price if it exists, and if not, create it
+                Parse.Cloud.run('getOrMakePrice', { species: req.params.species, category: req.params.category, cut: req.params.cut, units: req.params.unitType, price: req.params.pricePerUnit },
+                {
+                    success: function(priceRes)
+                    {
+                        // Now make the meat
+                        var theMeat = new Meat();
+                        theMeat.save({
+                            animal: animalRes.animal,
+                            cut: priceRes.price,
+                            location: "Unknown - new meat",
+                            units: req.params.units
+                        },
+                        {
+                            success: function(savedMeat)
+                            {
+                                res.success(savedMeat);
+                            },
+                            error: function(unsavedMeat, err)
+                            {
+                                res.error({ error: err.message, message: "Failed to save meat", meat: unsavedMeat });
+                            }
+                        });
+                    },
+                    error: function(err)
+                    {
+                        res.error({ error: err.message, message: "Failed to get/make price", cut: req.params.cut, units: req.params.unitType, price: req.params.pricePerUnit });
+                    }
+                });
+            },
+            error: function(err)
+            {
+                res.error({ error: err.message, message: "Failed to get/make animal", identifier: req.params.animalIdentifier, species: req.params.species, butcheryDate: req.params.butcheryDate });
+            }
+        });
+    }
+});
+
 // Function: moveMeatIDToFreezerID
 // @param {String} req.params.meat The ID of the meat object to move
 // @param {String} req.params.freezer The ID of the freezer object to move the meat into
--- cloud/prices.js
+++ cloud/prices.js
@@ -3,6 +3,67 @@ var Meat = Parse.Object.extend('Meat');
 var Animal = Parse.Object.extend('Animal');
 var Freezer = Parse.Object.extend('Freezer');
 
+
+// Function: getOrMakePrice
+// @param {String} req.params.species       The species of the meat, eg. "Beef" or "Chicken"
+// @param {String} req.params.cut           The cut of the meat, eg. "Rump Roast" or "Stewing Hen"
+// @param {String} [req.params.category]    The category of this cut, eg. "Roasts" or "Organic"
+// @param {String} [req.params.units]       The type of the units for this cut, eg. "lb" or "hen"
+// @param {Number} [req.params.price]       The price per unit, eg. 15.5 for "$15.50 per lb"
+//
+// Search for price matching {species, cut}; if not found, create new one with params data and return it
+Parse.Cloud.define('getOrMakePrice', function(req,res)
+{
+    // Same handler used for finding/creating by species/butcheryDate, or by identifier
+    handler_block = {
+                success: function(price)
+                {
+                    // Found, so return it
+                    res.success({ price: price });
+                },
+                error: function(err)
+                {
+                    // Not found, so create it
+                    var price = new Price();
+                    price.save({
+                        species: req.params.species,
+                        category: req.params.category,
+                        cut: req.params.cut,
+                        units: req.params.units,
+                        price: req.params.price
+                    },{
+                        success: function(price)
+                        {
+                            res.success({ price: price });
+                        },
+                        error: function(price, err)
+                        {
+                            res.error({ error: err, message: "Failed to save new price", price: price });
+                        }
+                    });
+                }
+            };
+
+    if(req.params.species === undefined)
+    {
+        res.json(400, new Parse.Error(Parse.Error.MISSING_OBJECT_ID, 'Failed to specify species'));
+    }
+    else if(req.params.cut === undefined)
+    {
+        res.json(400, new Parse.Error(Parse.Error.MISSING_OBJECT_ID, 'Failed to specify cut'));
+    }
+    else
+    {
+        // We have a species & butcheryDate -- search for it; if not found, create it
+        (new Parse.Query(Prices))
+            .equalTo("species",req.params.species)
+            .equalTo("cut", req.params.cut)
+            .first(handler_block);
+    }
+});
+
+
+
 module.exports = exports = function(app)
 {
 
@@ -12,15 +73,15 @@ module.exports = exports = function(app)
             (new Parse.Query('Prices'))
                 .ascending('-species,category,cut')
 
-            .find({
-                success: function(prices) {
-                    Parse.User.current().fetch();
-                    res.render('prices', { user:Parse.User.current(), prices: prices });
-                },
-                error: function(err) {
-                  res.json(500, err);
-                }
-            });
+                .find({
+                    success: function(prices) {
+                        Parse.User.current().fetch();
+                        res.render('prices', { user:Parse.User.current(), prices: prices });
+                    },
+                    error: function(err) {
+                      res.json(500, err);
+                    }
+                });
         } else {
             // User not logged in, redirect to login form.
             req.session.postLoginPage = '/prices';
