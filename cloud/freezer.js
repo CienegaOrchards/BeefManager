cc7b68eb04d33d8e6e051eff1a48916a7971f1cd
Craig R. Hughes <craig.git@rungie.com>
2015-01-09 23:58:36 -0800

    Simplify app routing/remove cloud code that's not being used

--- cloud/freezer.js
+++
@@ -1,9 +0,0 @@
-var Prices = Parse.Object.extend('Prices');
-var Meat = Parse.Object.extend('Meat');
-var Animal = Parse.Object.extend('Animal');
-var Freezer = Parse.Object.extend('Freezer');
-
-module.exports = exports = function(app)
-{
-    app.get('/inventory', function(req, res) { res.render('inventory'); });
-};
