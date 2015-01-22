b765740948cce0460cdaa018ac654a08212b2d35
Craig R. Hughes <craig.git@rungie.com>
2015-01-21 16:39:19 -0800

    Rename Inventory -> Buy

--- cloud/main.js
+++ cloud/main.js
@@ -4,7 +4,7 @@ var app = require('cloud/app.js');
 require('cloud/roles.js')(app);
 
 app.get('/'         , function(req, res) { res.render('index'); });
-app.get('/inventory', function(req, res) { res.render('inventory'); });
+app.get('/buy', function(req, res) { res.render('buy'); });
 app.get('/prices'   , function(req, res) { res.render('prices'); });
 app.get('/bulkEntry', function(req, res) { res.render('bulkEntry'); });
 app.get('/qrcodes'  , function(req, res) { res.render('qrcodes'); });
