42bb480064d36158f47c223709396003bc2a0c2c
Craig R. Hughes <craig.git@rungie.com>
2015-01-26 15:16:28 -0800

    alignment typo fix

--- cloud/main.js
+++ cloud/main.js
@@ -3,10 +3,10 @@ var app = require('cloud/app.js');
 
 require('cloud/roles.js')(app);
 
-app.get('/'         , function(req, res) { res.render('index'); });
-app.get('/buy', function(req, res) { res.render('buy'); });
-app.get('/prices'   , function(req, res) { res.render('prices'); });
-app.get('/bulkEntry', function(req, res) { res.render('bulkEntry'); });
-app.get('/qrcodes'  , function(req, res) { res.render('qrcodes'); });
+app.get('/'          , function(req , res) { res.render('index'); });
+app.get('/buy'       , function(req , res) { res.render('buy'); });
+app.get('/prices'    , function(req , res) { res.render('prices'); });
+app.get('/bulkEntry' , function(req , res) { res.render('bulkEntry'); });
+app.get('/qrcodes'   , function(req , res) { res.render('qrcodes'); });
 
 app.listen();
