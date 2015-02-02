c228ed949671da8976ff52046b5706ee7d1aa09a
Craig R. Hughes <craig.git@rungie.com>
2015-02-02 11:22:32 -0800

    Barcodes for freezers

--- cloud/main.js
+++ cloud/main.js
@@ -5,11 +5,12 @@ require('cloud/roles.js')(app);
 require('cloud/checkout.js')(app);
 require('cloud/stripeFunctions.js')(app);
 
-app.get('/'         , function(req, res) { res.render('index'); });
-app.get('/buy'      , function(req, res) { res.render('buy'); });
-app.get('/orders'   , function(req, res) { res.render('orders'); });
-app.get('/prices'   , function(req, res) { res.render('prices'); });
-app.get('/bulkEntry', function(req, res) { res.render('bulkEntry'); });
-app.get('/qrcodes'  , function(req, res) { res.render('qrcodes'); });
+app.get('/'          , function(req , res) { res.render('index'); });
+app.get('/buy'       , function(req , res) { res.render('buy'); });
+app.get('/orders'    , function(req , res) { res.render('orders'); });
+app.get('/prices'    , function(req , res) { res.render('prices'); });
+app.get('/bulkEntry' , function(req , res) { res.render('bulkEntry'); });
+app.get('/qrcodes'   , function(req , res) { res.render('qrcodes'); });
+app.get('/freezers'  , function(req , res) { res.render('freezers'); });
 
 app.listen();
